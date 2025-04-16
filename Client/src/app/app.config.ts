1c8280df21b8b9e4ec1e9ee5bda01432bcdd4510
Heba Anter <heba.anter18@gmail.com>
2025-04-17 01:39:49 +0200

    resolving changes

--- Client/src/app/app.config.ts
+++ Client/src/app/app.config.ts
@@ -3,12 +3,30 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { authInterceptor } from './interceptors/auth.interceptor';
-import { ReactiveFormsModule } from '@angular/forms'; // Add this import
-
+import { ReactiveFormsModule } from '@angular/forms';
+import {
+  SocialAuthServiceConfig,
+  GoogleLoginProvider,
+  GoogleInitOptions,
+} from '@abacritt/angularx-social-login';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '1031756027306-qt40so8h1a59955ra6huff4f835hn315.apps.googleusercontent.com' // Replace with your actual client ID
+            )
+          }
+        ]
+      } as SocialAuthServiceConfig,
+    }
   ]
 };
